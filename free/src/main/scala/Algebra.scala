4a492726a60a73f195d0fd97924df784038718ae
Alois Cochard <alois.cochard@gmail.com>
2016-01-20 18:38:52 +0100

    Add `Ap` in the `Free` algebra.

--- free/src/main/scala/Algebra.scala
+++ free/src/main/scala/Algebra.scala
@@ -13,16 +13,20 @@ object Algebra {
     def pure[A](f: Unit => A): Thunk =
       Map(_ => cast(f(()))) :: Nil
 
-    def bind[A](xs: Thunk)(f: A => Thunk): Thunk =
-      Bind(x => f(reify[A](x))) :: xs
+    def ap[A, B](xs: Thunk)(fab: Thunk): Thunk =
+      Ap(fab) :: xs
 
     def map[A, B](xs: Thunk)(f: A => B): Thunk =
       Map(x => cast(f(reify[A](x)))) :: xs
+
+    def bind[A](xs: Thunk)(f: A => Thunk): Thunk =
+      Bind(x => f(reify[A](x))) :: xs
   }
 
   sealed trait Op
 
   object Op {
+    case class Ap(fab: Thunk) extends Op
     case class Map(f: Val => Val) extends Op
     case class Bind(f: Val => Thunk) extends Op
   }
--- free/src/main/scala/Interpreter.scala
+++ free/src/main/scala/Interpreter.scala
@@ -9,16 +9,22 @@ import System.Val
 object Interpreter {
   def eval(thunk0: Thunk): Val = {
     @tailrec
-    def go(thunk: List[Op], value: Val): Val =
+    def go(thunk: List[Op], value: Val, stack: List[List[Op]]): Val =
       thunk.headOption match {
-        case None => value
+        case None => stack.headOption  match {
+          case None => value
+          case Some(thunk0) =>
+            val f = Val.reify[Val => Val](value)
+            go(thunk, f(value), stack.tail)
+        }
         case Some(op) =>
           op match {
-            case Op.Map(f) => go(thunk.tail, f(value))
-            case Op.Bind(f) => go(f(value).reverse, Val.unit)
+            case Op.Ap(fab) => go(fab.reverse, value, thunk::stack)
+            case Op.Map(f) => go(thunk.tail, f(value), stack)
+            case Op.Bind(f) => go(f(value).reverse, Val.unit, stack)
           }
       }
 
-    go(thunk0.reverse, Val.unit)
+    go(thunk0.reverse, Val.unit, Nil)
   }
 }
