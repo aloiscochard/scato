5e8e6e07f3bdf5a4ae8046edc748cefd35a7a34f
Alois Cochard <alois.cochard@gmail.com>
2016-01-21 09:24:06 +0100

    Add `Traversable` and `Disjunction` in prelude.

--- prelude/src/main/scala/Prelude.scala
+++ prelude/src/main/scala/Prelude.scala
@@ -4,7 +4,9 @@ import clazz._
 
 import scala.language.implicitConversions
 
-trait Prelude {
+trait Prelude extends data.DisjunctionFunctions {
+  type \/[L, R] = data.Disjunction.\/[L, R]
+
   // FunctorSyntax
   implicit def PfunctorOps[F[_], A](fa: F[A])(implicit F: TC[F, Functor]): FunctorSyntax.Ops[F, A] =
     new FunctorSyntax.Ops(fa)(F.instance)
@@ -28,6 +30,13 @@ trait Prelude {
 
   // ApplicativeSyntax
   implicit def PapplicativeOpsA[A](a: A): ApplicativeSyntax.OpsA[A] = new ApplicativeSyntax.OpsA(a)
+
+  // TraversableSyntax
+  implicit def traversableOps[T[_], A](ta: T[A])(implicit T: TC[T, Traversable]): TraversableSyntax.Ops[T, A] =
+    new TraversableSyntax.Ops(ta)(T.instance)
+
+  implicit def traversable[T[_], TA](ta: TA)(implicit T: TCU[Traversable, TA]): TraversableSyntax.Ops[T.T, T.A] =
+    new TraversableSyntax.Ops(T(ta))(T.instance)
 }
 
 object Prelude extends Prelude
