5cc4dd6962a57bd44f43f719e98d1f113cb08639
Alois Cochard <alois.cochard@gmail.com>
2016-01-21 10:18:21 +0100

    Promote `FreeInstances` to `TC` with caching.

--- free/src/main/scala/monad/FreeInstances.scala
+++ free/src/main/scala/monad/FreeInstances.scala
@@ -5,16 +5,18 @@ package monad
 import clazz.{Apply, Applicative, Bind, BindRec, Functor, Monad}
 
 trait FreeInstances {
-  implicit def freeBind[F[_]](implicit F: TC[F, Functor]): Bind[Free[F, ?]] = new Bind[Free[F, ?]] {
-    override val apply = new Apply[Free[F, ?]] {
-      override val functor = new Functor[Free[F, ?]] {
-        override def map[A, B](fa: Free[F, A])(f: A => B): Free[F, B] = fa.map(f)
-      }
-      override def ap[A, B](fa: Free[F, A])(fab: Free[F, A => B]): Free[F, B] = fa.ap(fab)
-    }
 
-    override def flatMap[A, B](fa: Free[F, A])(fb: A => Free[F, B]): Free[F, B] = fa.flatMap(fb)
-  }
+  implicit def freeBind[F[_]](implicit F: TC[F, Functor]): TC[Free[F, ?], Bind] =
+    TC.capture[Free[F, ?], Bind, Free[Any, ?]](new Bind[Free[F, ?]] {
+      override val apply = new Apply[Free[F, ?]] {
+        override val functor = new Functor[Free[F, ?]] {
+          override def map[A, B](fa: Free[F, A])(f: A => B): Free[F, B] = fa.map(f)
+        }
+        override def ap[A, B](fa: Free[F, A])(fab: Free[F, A => B]): Free[F, B] = fa.ap(fab)
+      }
+
+      override def flatMap[A, B](fa: Free[F, A])(fb: A => Free[F, B]): Free[F, B] = fa.flatMap(fb)
+    })
 
   // TODO Implement bindRec instance
   /*
@@ -25,12 +27,13 @@ trait FreeInstances {
   }
   */
 
-  implicit def freeMonad[F[_]](implicit F: TC[F, Functor]): Monad[Free[F, ?]] = new Monad[Free[F, ?]] {
-    override val bind = freeBind[F]
-    override val applicative = new Applicative[Free[F, ?]] {
-      override val apply = bind.apply
-      override def pure[A](a: A): Free[F, A] = Free(Algebra.Thunk.pure[A](_ => a))
-    }
-  }
+  implicit def freeMonad[F[_]](implicit F: TC[F, Functor]): TC[Free[F, ?], Monad] =
+    TC.capture[Free[F, ?], Monad, Free[Any, ?]](new Monad[Free[F, ?]] {
+      override val bind = freeBind[F].instance
+      override val applicative = new Applicative[Free[F, ?]] {
+        override val apply = freeBind[F].instance.apply
+        override def pure[A](a: A): Free[F, A] = Free(Algebra.Thunk.pure[A](_ => a))
+      }
+    })
 }
 
