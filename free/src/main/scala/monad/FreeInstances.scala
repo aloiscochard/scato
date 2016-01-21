41cee14eb97a8f69e29186594b15470dc98276be
Alois Cochard <alois.cochard@gmail.com>
2016-01-21 23:44:06 +0100

    Disable caching on `Disjunction/FreeInstances`.

--- free/src/main/scala/monad/FreeInstances.scala
+++ free/src/main/scala/monad/FreeInstances.scala
@@ -7,7 +7,8 @@ import clazz.{Apply, Applicative, Bind, BindRec, Functor, Monad}
 trait FreeInstances {
 
   implicit def freeBind[F[_]](implicit F: TC[F, Functor]): TC[Free[F, ?], Bind] =
-    TC.capture[Free[F, ?], Bind, Free[Any, ?]](new Bind[Free[F, ?]] {
+    //TC.capture[Free[F, ?], Bind, Free[Any, ?]](new Bind[Free[F, ?]] {
+    TC[Free[F, ?], Bind](new Bind[Free[F, ?]] {
       override val apply = new Apply[Free[F, ?]] {
         override val functor = new Functor[Free[F, ?]] {
           override def map[A, B](fa: Free[F, A])(f: A => B): Free[F, B] = fa.map(f)
@@ -28,7 +29,8 @@ trait FreeInstances {
   */
 
   implicit def freeMonad[F[_]](implicit F: TC[F, Functor]): TC[Free[F, ?], Monad] =
-    TC.capture[Free[F, ?], Monad, Free[Any, ?]](new Monad[Free[F, ?]] {
+    // TC.capture[Free[F, ?], Monad, Free[Any, ?]](new Monad[Free[F, ?]] {
+    TC[Free[F, ?], Monad](new Monad[Free[F, ?]] {
       override val bind = freeBind[F].instance
       override val applicative = new Applicative[Free[F, ?]] {
         override val apply = freeBind[F].instance.apply
