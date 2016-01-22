04982ab1d2df080c5614fc7ba40de795f9678ec7
Alois Cochard <alois.cochard@gmail.com>
2016-01-22 09:04:53 +0100

    Fix `Val.cast` type parameter usage.

--- base/src/main/scala/system/Unsafe.scala
+++ base/src/main/scala/system/Unsafe.scala
@@ -6,7 +6,7 @@ object Unsafe {
 
   object Val {
     val unit: Val = cast(Unit)
-    def cast[A](x: Any): Val = x.asInstanceOf[Val]
+    def cast[A](x: A): Val = x.asInstanceOf[Val]
     def castF[F[_], A](x: F[A]): F[Val] = x.asInstanceOf[F[Val]]
     def reify[A](x: Val): A = x.asInstanceOf[A]
     def reifyF[F[_], A](x: F[Val]): F[A] = x.asInstanceOf[F[A]]
