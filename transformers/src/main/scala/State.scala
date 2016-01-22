cf52ef4130e2dd36bbc7de1334ed00743330fa16
Alois Cochard <alois.cochard@gmail.com>
2016-01-22 20:24:51 +0100

    Add `State.pure/run`.

--- transformers/src/main/scala/State.scala
+++ transformers/src/main/scala/State.scala
@@ -2,6 +2,8 @@ package scato
 package transformers
 
 object State {
+  def pure[S, A](a: A): State[S, A] = StateT.pure[S, Identity, A](a)
   def state[S, A](f: S => (A, S)): State[S, A] = StateT.state[S, Identity, A](f)
+  def run[S, A](sa: StateT[S, Identity, A])(s: S): (A, S) = StateT.run[S, Identity, A](sa)(s).run
 }
 
