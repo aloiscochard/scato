9278c73c7f2100662b93cb40ef81536aae8fd7f5
Alois Cochard <alois.cochard@gmail.com>
2016-01-23 11:58:52 +0100

    Rewrite StdLib `Monad` instances using `Class` templates.

--- base/src/main/scala/clazz/MonadInstancesStdLib.scala
+++ base/src/main/scala/clazz/MonadInstancesStdLib.scala
@@ -2,37 +2,23 @@ package scato
 package clazz
 
 trait MonadInstancesStdLib {
-  implicit val option: Monad[Option] = new Monad[Option] {
-    override val applicative = new Applicative[Option] {
-      override val apply = new Apply[Option] {
-        override val functor = new Functor[Option] {
-          override def map[A, B](oa: Option[A])(f: A => B): Option[B] = oa.map(f)
-        }
-        override def ap[A, B](oa: Option[A])(f: Option[A => B]): Option[B] = oa.flatMap(a => f.map(_(a)))
-      }
-      override def pure[A](a: A): Option[A] = Some(a)
-    }
-
-    override val bind = new Bind[Option] {
-      override def apply = applicative.apply
+  implicit val option: Monad[Option] = {
+    val clazz = new MonadClass[Option] {
+      override def ap[A, B](oa: Option[A])(f: Option[A => B]): Option[B] = oa.flatMap(a => f.map(_(a)))
       override def flatMap[A, B](oa: Option[A])(f: A => Option[B]): Option[B] = oa.flatMap(f)
+      override def map[A, B](oa: Option[A])(f: A => B): Option[B] = oa.map(f)
+      override def pure[A](a: A): Option[A] = Option(a)
     }
+    clazz.monad
   }
 
-  implicit val list: Monad[List] = new Monad[List] {
-    override val applicative = new Applicative[List] {
-      override val apply = new Apply[List] {
-        override val functor = new Functor[List] {
-          override def map[A, B](xs: List[A])(f: A => B): List[B] = xs.map(f)
-        }
-        override def ap[A, B](xs: List[A])(f: List[A => B]): List[B] = xs.flatMap(a => f.map(_(a)))
-      }
+  implicit val list: Monad[List] = {
+    val clazz = new MonadClass[List] {
+      override def ap[A, B](xs: List[A])(f: List[A => B]): List[B] = xs.flatMap(a => f.map(_(a)))
+      override def flatMap[A, B](xs: List[A])(f: A => List[B]): List[B] = xs.flatMap(f)
+      override def map[A, B](xs: List[A])(f: A => B): List[B] = xs.map(f)
       override def pure[A](a: A): List[A] = List(a)
     }
-
-    override val bind = new Bind[List] {
-      override def apply = applicative.apply
-      override def flatMap[A, B](xs: List[A])(f: A => List[B]): List[B] = xs.flatMap(f)
-    }
+    clazz.monad
   }
 }
