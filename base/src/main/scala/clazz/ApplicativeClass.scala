e11725753003fceb43915ad7d5b1ef75cad4455d
Alois Cochard <alois.cochard@gmail.com>
2016-01-23 12:14:01 +0100

    Add default `map` implementation for `ApplicativeClass`.

--- base/src/main/scala/clazz/ApplicativeClass.scala
+++ base/src/main/scala/clazz/ApplicativeClass.scala
@@ -6,6 +6,6 @@ trait ApplicativeClass[F[_]] extends ApplyClass[F] { self =>
     def apply = self.apply
     def pure[A] (a: A): F[A] = self.pure[A](a)
   }
-
+  override def map[A, B](fa: F[A])(f: A => B): F[B] = ap(fa)(pure(f))
   def pure[A](a: A): F[A]
 }
