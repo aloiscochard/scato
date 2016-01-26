69e594f2c289e78658521dc9095400d5add5aa37
Alois Cochard <alois.cochard@gmail.com>
2016-01-26 09:52:42 +0100

    Move `StateT.exec/run` in the data type.

--- transformers/src/main/scala/State.scala
+++ transformers/src/main/scala/State.scala
@@ -4,6 +4,6 @@ package transformers
 object State {
   def pure[S, A](a: A): State[S, A] = StateT.pure[S, Identity, A](a)
   def state[S, A](f: S => (A, S)): State[S, A] = StateT.state[S, Identity, A](f)
-  def run[S, A](sa: StateT[S, Identity, A])(s: S): (A, S) = StateT.run[S, Identity, A](sa)(s).run
+  def run[S, A](sa: StateT[S, Identity, A])(s: S): (A, S) = sa.run(s).run
 }
 
