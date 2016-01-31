754c483eaba9642153fa590f6a5c44ffe21f92c7
Alois Cochard <alois.cochard@gmail.com>
2016-01-31 18:42:26 +0100

    Rename `S` to `F` type parameter.
    It does represent a `Functor`.

--- free/src/main/scala/monad/Free.scala
+++ free/src/main/scala/monad/Free.scala
@@ -5,12 +5,12 @@ package monad
 import clazz.Functor
 
 object Free {
-  def pure[S[_], A](a: A): FreeT[S, Identity, A] =
-    FreeT.pure[S, Identity, A](a)
+  def pure[F[_], A](a: A): FreeT[F, Identity, A] =
+    FreeT.pure[F, Identity, A](a)
 
-  def liftF[S[_], A](sa: S[A])(implicit S: Functor[S]): FreeT[S, Identity, A] =
-    FreeT.liftF[S, Identity, A](sa)
+  def liftF[F[_], A](sa: F[A])(implicit F: Functor[F]): FreeT[F, Identity, A] =
+    FreeT.liftF[F, Identity, A](sa)
 
-  def run[S[_], A](free: FreeT[S, Identity, A])(f: S[A] => A)(implicit S: Functor[S]): A =
-    FreeT.run[S, Identity, A](free)(sma => Identity(f(S.map(sma)(_.run)))).run
+  def run[F[_], A](free: FreeT[F, Identity, A])(f: F[A] => A)(implicit F: Functor[F]): A =
+    FreeT.run[F, Identity, A](free)(sma => Identity(f(F.map(sma)(_.run)))).run
 }
